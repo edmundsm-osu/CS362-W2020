47e5b1350e47af6e28d8cf1e23c892e8f6b6d1b7
Alex <edmundsm@oregonstate.edu>
2020-01-19 13:14:50 -0500

    Added testDominion1 and 2 files, refactored playDominion.py, playDominion.py now depends on testUtility.py

---
+++ projects/edmundsm/dominion/testDominion1.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Modified on Wed Jan 15 12:51 2020
+
+@author: Martin Edmunds
+"""
+
+# import Dominion
+# import random
+# from collections import defaultdict
+
+from testUtility import *
+
+# Get player names
+player_names = ["*Annie","*Ben","*Carla"]
+
+# calculate number of victory cards and curses
+nV = GetVictoryCount(player_names)
+nC = GetCurseCount(player_names)
+
+# Select the possible cards to be included in the game
+box = SetupBoxes(nV)
+
+# Set up the supply
+supply_order = SetSupplyOrder()
+supply = PopulateSupply(box, nV, nC, len(player_names))
+
+# initialize the trash
+trash = []
+
+# initialize player objects
+players = ConstructPlayers(player_names)
+
+# testing overrides
+
+# start game loop
+PlayGame(supply, supply_order, players, trash)
+
+# calculate final score
+GetScore(players)
